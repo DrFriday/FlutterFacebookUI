2ac74f8a85bfcddf0390ab314a8c4fc79e111517
DrFriday <kafleysaagar@gmail.com>
2022-07-27 12:12:56 +0545

    stories added

--- facebookui/lib/main.dart
+++ facebookui/lib/main.dart
@@ -45,10 +45,131 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                 ),
+                const SizedBox(
+                  width: 20,
+                ),
+                const Icon(
+                  Icons.camera_alt,
+                  color: Color.fromARGB(255, 52, 52, 52),
+                  size: 30,
+                ),
               ],
             ),
-          )
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.vertical,
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.baseline,
+                      textBaseline: TextBaseline.alphabetic,
+                      children: const <Widget>[
+                        Text(
+                          "Stories",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 100, 100, 100),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            letterSpacing: 1.1,
+                          ),
+                        ),
+                        Text(
+                          "See Archives",
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 100, 100, 100),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      height: 180,
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: <Widget>[
+                          makeStory(
+                              storyImage: 'assets/images/story/story-1.jpg',
+                              userImage: 'assets/images/p1.jpg',
+                              userName: 'Rami Malik'),
+                          makeStory(
+                              storyImage: 'assets/images/story/story-2.jpg',
+                              userImage: 'assets/images/p2.jpg',
+                              userName: 'User 2'),
+                          makeStory(
+                              storyImage: 'assets/images/story/story-3.jpg',
+                              userImage: 'assets/images/p3.jpg',
+                              userName: 'User 3'),
+                          makeStory(
+                              storyImage: 'assets/images/story/story-4.jpg',
+                              userImage: 'assets/images/p4.jpg',
+                              userName: 'User 4'),
+                          makeStory(
+                              storyImage: 'assets/images/story/story-5.jpg',
+                              userImage: 'assets/images/p5.jpg',
+                              userName: 'User 5'),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+
+  Widget makeStory({storyImage, userImage, userName}) {
+    return AspectRatio(
+      aspectRatio: 1.6 / 2,
+      child: Container(
+        margin: const EdgeInsets.only(right: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image:
+              DecorationImage(image: AssetImage(storyImage), fit: BoxFit.cover),
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(15),
+            gradient: LinearGradient(
+              begin: Alignment.bottomRight,
+              colors: [
+                Colors.black.withOpacity(0.9),
+                Colors.black.withOpacity(0.1),
+              ],
+            ),
+          ),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Container(
+                  width: 40,
+                  height: 40,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white, width: 2),
+                    image: DecorationImage(
+                        image: AssetImage(userImage), fit: BoxFit.cover),
+                  ),
+                ),
+                Text(
+                  userName,
+                  style: const TextStyle(
+                      color: Color.fromARGB(255, 255, 255, 255)),
+                ),
+              ]),
+        ),
       ),
     );
   }
